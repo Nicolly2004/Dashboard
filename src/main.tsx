61ee6e1c1f46b0da08d9a2eea5c2b37543bc2829
Nicolly Reis <nicollyro2004@gmail.com>
2023-04-12 22:04:00 -0300

    Implementa React hook form

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import { routes } from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 .render(
